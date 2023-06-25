88e355241968eb3896350c061779518df549116d
Neha <nehanupur497@gmail.com>
2023-06-26 03:25:45 +0530

    Add Contact Components

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import "./css/style.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
